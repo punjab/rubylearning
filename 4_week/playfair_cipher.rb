011edea786d100535d5bdc87b902a10591577871
Arvinder Singh Kang <punjcoder@gmail.com>
2014-08-19 14:40:15 -0700

    Removed a bug. Seperated data structure from display

--- 4_week/playfair_cipher.rb
+++ 4_week/playfair_cipher.rb
@@ -7,34 +7,42 @@
   Licensed under the MIT license
 =end
 
-=begin
-  doctest: Create a 5x5 table for "playfair example"
-  >> create_table("playfair example")
-  =>P L A Y F 
-    I R E X M 
-    B C D G H 
-    K N O Q S 
-    T U V W Z 
-  doctest: Create a 5x5 table for "I Love Ruby."
-  >> create_table("I Love Ruby.")
-  =>I L O V E 
-    R U B Y A 
-    C D F G H 
-    K M N P Q 
-    S T W X Z 
-=end
-def create_table(text)
-  result = ''
+
+def matrix(text)
+  square = Array.new(5){Array.new(5,0)} 
   letters = text.upcase.scan(/[A-Za-z]/)
   alphabets = letters + (('A'..'Z').to_a - letters)
-  j_index = alphabets.index('J')
-  alphabets[j_index] = 'I'
+  alphabets.each.with_index {|e,i| alphabets[i] = 'I' if e == 'J'}
   alphabets.uniq!
-  5.times do
-    5.times do
-      result << alphabets.shift << " "
+  5.times do |i|
+    5.times do |j|
+      square[i][j] = alphabets.shift
     end
-    result << "\n"
+  end
+  square
+end
+
+=begin
+  doctest: Create a 5x5 table for "playfair example"
+  >> display_table("playfair example")
+  =>P L A Y F
+    I R E X M
+    B C D G H
+    K N O Q S
+    T U V W Z
+  doctest: Create a 5x5 table for "I Love Ruby."
+  >> display_table("I Love Ruby.")
+  =>I L O V E
+    R U B Y A
+    C D F G H
+    K M N P Q
+    S T W X Z
+=end
+def display_table(text)
+  result = ''
+  square = matrix(text)
+  5.times do |i|
+    result << square[i].join(' ') << '\n'
   end
   result
 end
@@ -65,12 +73,12 @@ end
 
 =begin
   doctest: prepare message "Congress shall"
-  >> prepare_message("Congress shall")
+  >> display_message("Congress shall")
   => "CO NG RE SX SZ SH AL LX"
   doctest: prepare message "Hide the gold in the tree stump"
-  >> prepare_message("Hide the gold in the tree stump")
+  >> display_message("Hide the gold in the tree stump")
   => "HI DE TH EG OL DI NT HE TR EX ES TU MP"
 =end
-def prepare_message(text)
+def display_message(text)
   make_pairs(scan_for_indentical(text)).join(' ')
 end
