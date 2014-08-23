490828d1f8e89e8d12c73312f1a352673229f471
Arvinder Singh Kang <punjcoder@gmail.com>
2014-08-23 14:45:36 -0700

    update prompt to module and generated edoc

--- 5_week/1c_quick_and_dirty_repl.rb
+++ 5_week/1c_quick_and_dirty_repl.rb
@@ -1,33 +1,31 @@
 #!/usr/bin/env ruby -wKU
+
 =begin
-  Name: 1c_quick_and_dirty_repl.rb
-  Description: Quick and easy prompt
-  Author: Arvinder Kang
-  Date: 2014-08-21
-  Licensed under the MIT license
+  * Name: 1c_quick_and_dirty_repl.rb
+  * Description: Quick and easy prompt
+  * Author: Arvinder Kang
+  * Date: 2014-08-21
+  * Licensed under the MIT license
 =end
 
-if ARGV.first == '--help'
-  puts <<END
-
-  Usage: ruby 1c_quick_and_dirty_repl.rb [s]
-
-    where
-      s :  is the number of seconds to wait before launch. Defaults to 3.
-
-END
-  exit
-end
-
-class Repl
-  def countdown(seconds=3)
+module Repl
+  
+  # Default 3 second counter before launching REPL
+  #
+  # * Optional, can be disabled by setting parameter seconds to zero.
+  
+  def self.countdown(seconds=3)
     counter = seconds
     print "Launchin in ..."
     seconds.times { print "#{counter}..."; counter -= 1; sleep 1}
     puts
   end
   
-  def ask(input)
+  # Evaluates the input string and returns the output or error.
+  #
+  # * The result is ANSI colored.
+  # * If terminal support less than 8 colors, color is stripped out.
+  def self.ask(input)
     begin
       output ||= eval(input)
       result = "#=> \033[37m#{output}\033[0m"
@@ -41,16 +39,24 @@ end
 
 
 if __FILE__ == $0
-  repl = Repl.new
-  ARGV.empty? ? repl.countdown : repl.countdown(ARGV[0].to_i)
+  
+  if ARGV.first == '--help'
+    puts <<END
+    Usage: ruby 1c_quick_and_dirty_repl.rb [s]
+      where
+        s :  is the number of seconds to wait before launch. Defaults to 3.
+END
+    exit
+  end
+  
+  ARGV.empty? ? Repl.countdown : Repl.countdown(ARGV[0].to_i)
   puts "Welcome to MY-RB. You are using #{`ruby -v`.chomp}. Have fun ;)"
   response = ''
   until response =~ /^exit$/
     print ">> "
     input = STDIN.gets
-    response = repl.ask(input)
+    response = Repl.ask(input)
     puts response
   end
   puts "You are exiting our REPL"
 end
-
