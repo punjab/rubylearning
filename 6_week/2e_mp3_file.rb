6e050c0482bd4737c8fe2b767d2128de57615376
Arvinder Singh Kang <punjcoder@gmail.com>
2014-08-23 13:46:47 -0700

    Week 6 ex2 done

--- 6_week/2e_mp3_file.rb
+++ 6_week/2e_mp3_file.rb
@@ -11,11 +11,26 @@ class MP3
   def initialize(song)
     @song = song
   end
-  def id3
-    @tag = IO.read(@song)[-128..-1]
-    @tag.unpack('xxxA30')
+  
+  def id3_tags
+    if tag_present?
+      tags = []
+      last_125_bytes = IO.read(@song)[-125..-1]
+      last_125_bytes.force_encoding('UTF-8').scan(/.{30}/).each do |tag|
+        tags << tag.unpack('A30')[0]
+      end
+      return tags
+    end
+    return "No TAG id3 tag found!"
   end
+  
+  private
+    def tag_present?
+      IO.read(@song)[-128..-126] == 'TAG'
+    end
 end
 
 song = MP3.new('song.mp3')
-p song.id3
+p song.id3_tags
+
+
