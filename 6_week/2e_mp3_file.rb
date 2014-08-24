4601ae61e788f50ccf159cefc6e67d66befdc30e
Arvinder Singh Kang <punjcoder@gmail.com>
2014-08-24 16:25:01 -0700

    updated mp3 and text analyzers

--- 6_week/2e_mp3_file.rb
+++ 6_week/2e_mp3_file.rb
@@ -13,9 +13,93 @@ class MP3
     artist: 30,
     album: 30,
     year: 4,
-    comment: 30
+    comment: 28,
+    track_number: 2,
+    genre: 1
   }
   
+  GENRE = [
+  'Classic Rock',
+  'Country',
+  'Dance',
+  'Disco',
+  'Funk',
+  'Grunge',
+  'Hip-Hop',
+  'Jazz',
+  'Metal',
+  'New Age',
+  'Oldies',
+  'Other',
+  'Pop',
+  'R&B',
+  'Rap',
+  'Reggae',
+  'Rock',
+  'Techno',
+  'Industrial',
+  'Alternative',
+  'Ska',
+  'Death Metal',
+  'Pranks',
+  'Soundtrack',
+  'Euro-Techno',
+  'Ambient',
+  'Trip-Hop',
+  'Vocal',
+  'Jazz+Funk',
+  'Fusion',
+  'Trance',
+  'Classical',
+  'Instrumental',
+  'Acid',
+  'House',
+  'Game',
+  'Sound Clip',
+  'Gospel',
+  'Noise',
+  'AlternRock',
+  'Bass',
+  'Soul',
+  'Punk',
+  'Space',
+  'Meditative',
+  'Instrumental Pop',
+  'Instrumental Rock',
+  'Ethnic',
+  'Gothic',
+  'Darkwave',
+  'Techno-Industrial',
+  'Electronic',
+  'Pop-Folk',
+  'Eurodance',
+  'Dream',
+  'Southern Rock',
+  'Comedy',
+  'Cult',
+  'Gangsta Rap',
+  'Top 40',
+  'Christian Rap',
+  'Pop / Funk',
+  'Jungle',
+  'Native American',
+  'Cabaret',
+  'New Wave',
+  'Psychedelic',
+  'Rave',
+  'Showtunes',
+  'Trailer',
+  'Lo-Fi',
+  'Tribal',
+  'Acid Punk',
+  'Acid Jazz',
+  'Polka',
+  'Retro',
+  'Musical',
+  'Rock & Roll',
+  'Hard Rock'
+  ]
+
   def initialize(song)
     @song = song
   end
@@ -23,22 +107,41 @@ class MP3
   def id3_tags
     if tag_present?
       metadata = read_metadata
-      index, tags = 0, {}
-      SCHEMA.each do |tag_name, tag_length|
-        tags[tag_name] = metadata.byteslice(index, tag_length).strip
-        index += tag_length
-      end
-      tags
+      @tags = {}
+      parse(metadata)
+      @tags[:track_number] = parse_track_tag
+      @tags[:genre] = parse_genre_tag
     end
+    @tags
   end
 
   private
+    def read_metadata
+      IO.read(@song)[-125..-1]
+    end
+    
     def tag_present?
       IO.read(@song)[-128..-126] == 'TAG'
     end
+  
+    def parse(metadata)
+      index = 0
+      SCHEMA.each do |tag_name, tag_length|
+        tag_value = metadata.byteslice(index, tag_length)
+        tag_value.strip! unless tag_name == :track_number
+        @tags[tag_name] = tag_value.empty? ? "No Information Known" : tag_value
+        index += tag_length
+      end
+    end
     
-    def read_metadata
-      IO.read(@song)[-125..-1]
+    def parse_track_tag
+      zero_byte, track = @tags[:track_number].unpack('c*')
+      zero_byte == 0 ? track : nil
+    end
+    
+    def parse_genre_tag
+      genre_code = @tags[:genre].unpack('c*')[0]
+      GENRE[genre_code-1]
     end
 end
 
--- 6_week/4e_text_analyser_exception.rb
+++ 6_week/4e_text_analyser_exception.rb
@@ -7,54 +7,62 @@
   Licensed under the MIT license
 =end
 
-class Analyzer
-  def analyze_text(text)
-    {
-      :line_count => (text.split(/\n/).last.chomp.empty? ? text.scan(/\n/).count + 1 : text.scan(/\n/).count),
-      :character_count => text.length,
-      :character_count_excluding_spaces => text.scan(/\S/).length,
-      :word_count => text.split(' ').length,
-      :sentence_count => text.split(/[\.?!]/).length,
-      :paragraph_count => text.split(/\n\r/).length
-    }
+module TextAnalyzer
+  
+  require 'logger'
+
+  class Analyzer
+    def analyze_text(text)
+      {
+        :line_count => (text.split(/\n/).last.chomp.empty? ? text.scan(/\n/).count + 1 : text.scan(/\n/).count),
+        :character_count => text.length,
+        :character_count_excluding_spaces => text.scan(/\S/).length,
+        :word_count => text.split(' ').length,
+        :sentence_count => text.split(/[\.?!]/).length,
+        :paragraph_count => text.split(/\n\r/).length
+      }
+    end
+
+    def report(analysis_result)
+      output = <<REPORT
+      ***********************
+          Analysis Report    
+      ***********************
+REPORT
+      analysis_result.each do |key, value|
+        output << "#{key.to_s.gsub(/_/, ' ').capitalize} : #{value}\n"
+      end
+      output << "Average number of words per sentence: %.2f \n" % 
+                (analysis_result[:word_count]/analysis_result[:sentence_count].to_f)
+      output << "Average number of sentences per paragraph: %.2f \n" % 
+                (analysis_result[:sentence_count]/analysis_result[:paragraph_count].to_f)
+      output
+    end
   end
 
-  def report(analysis_result)
-    output = <<REPORT
-    ***********************
-        Analysis Report    
-    ***********************
-REPORT
-    analysis_result.each do |key, value|
-      output << "#{key.to_s.gsub(/_/, ' ').capitalize} : #{value}\n"
+  def self.start
+    print "Please provide a name for log file. Default (log_file.log): "
+    logfile = gets.chomp
+    logfile = 'log_file.log' if logfile.empty?
+    $LOG = Logger.new(logfile, 'monthly')
+    $LOG.level = Logger::INFO
+
+    print "File to be analyzed? "
+    filename = gets.chomp
+    begin
+      corpora = IO.read(filename)
+      a = Analyzer.new
+      analysis_result = a.analyze_text(corpora)
+      puts a.report(analysis_result)
+      $LOG.info("Analysis report on #{filename} successful.")
+    rescue Exception => e
+      $LOG.error "#{e.class} error occured: #{e.message}"
+      puts "#{e.class} error Occured. Refer log file for details!"
     end
-    output << "Average number of words per sentence: %.2f \n" % 
-              (analysis_result[:word_count]/analysis_result[:sentence_count].to_f)
-    output << "Average number of sentences per paragraph: %.2f \n" % 
-              (analysis_result[:sentence_count]/analysis_result[:paragraph_count].to_f)
-    output
-  end
+  end 
 end
 
-
 begin
-  require 'logger'
-  print "Please provide a name for log file. Default (log_file.log): "
-  logfile = gets.chomp
-  logfile = 'log_file.log' if logfile.empty?
-  $LOG = Logger.new(logfile, 'monthly')
-  $LOG.level = Logger::INFO
-  
-  print "File to be analyzed? "
-  filename = gets.chomp
-  begin
-    corpora = IO.read(filename)
-    a = Analyzer.new
-    analysis_result = a.analyze_text(corpora)
-    puts a.report(analysis_result)
-    $LOG.info("Analysis report on #{filename} successful.")
-  rescue Exception => e
-    $LOG.error "#{e.class} error occured: #{e.message}"
-    puts "#{e.class} error Occured. Refer log file for details!"
-  end
-end if  __FILE__ == $0
+  TextAnalyzer.start
+end  if  __FILE__ == $0
+
